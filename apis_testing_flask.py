c6967e9a60516c2c430f92e05edada9b6c89e319
Rachel Winkler <rachelwinkler9@gmail.com>
2019-01-25 10:45:03 +0000

    added user input

--- apis_testing_flask.py
+++ apis_testing_flask.py
@@ -7,6 +7,7 @@ Created on Mon Jan 21 09:38:29 2019
 import requests
 import random
 import config
+import app
 
 
 def choose_extra_value():
@@ -80,6 +81,30 @@ def generate_advice():
     print(advice)
     return advice
 
+def generate_pet(user_animal):
+    pet = user_animal
+    print(pet)
+    return pet
+
+def choose_visitor(user_number):
+    user_number = int(user_number)
+    while user_number > 6:
+        user_number = user_number % 2
+    if user_number ==1:
+        visitor = "wizard"
+    elif user_number ==2:
+        visitor = "dragon"
+    elif user_number ==3:
+        visitor = "mysterious floating figure clothed in black"
+    elif user_number ==4:
+        visitor = "witch"
+    elif user_number ==5:
+        visitor = "unicorn"
+    elif user_number ==6:
+        visitor = "elf"
+        print(visitor)
+    return visitor
+
 #Other values for FakeJSON:
 #          "companyName": "companyName",
 #          "companyDepartment": "companyDepartment",
@@ -95,11 +120,13 @@ def generate_advice():
 ##############
 
 
-extraValue = choose_extra_value()
-protagonist = generate_user(extraValue)
-advice = generate_advice()
-smallInt = no_of_pets(protagonist)
-fName = first_name(protagonist)
-lName = last_name(protagonist)
-city = city(protagonist)
-job_title = job_title(protagonist)
+# extraValue = choose_extra_value()
+# protagonist = generate_user(extraValue)
+# advice = generate_advice()
+# smallInt = no_of_pets(protagonist)
+# fName = first_name(protagonist)
+# lName = last_name(protagonist)
+# city = city(protagonist)
+# job_title = job_title(protagonist)
+# pet = generate_pet(animal)
+# visitor = choose_vistor(user_number)
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import apis_testing_flask as atf
+from apis_testing_flask import *
 
 app = Flask(__name__)
 
@@ -7,9 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/generate")
+@app.route("/generate", methods=["POST"] )
 def generate():
-    return render_template("generate.html", advice=atf.advice, smallInt=atf.smallInt, fName=atf.fName, lName=atf.lName, city=atf.city, job_title=atf.job_title)
+    form_data = request.form
+    user_animal = form_data["animal"]
+    user_number = form_data["number"]
+    extraValue = choose_extra_value()
+    protagonist = generate_user(extraValue)
+    advice = generate_advice()
+    smallInt = no_of_pets(protagonist)
+    fName = first_name(protagonist)
+    lName = last_name(protagonist)
+    ccity = city(protagonist)
+    jjob_title = job_title(protagonist)
+    pet = generate_pet(user_animal)
+    vvisitor = choose_visitor(user_number)
+    return render_template("generate.html", **locals())
 
 if __name__ == "__main__":
     app.run(debug=True)
